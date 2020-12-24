232b9aa322f720d7421638be632050dfd12715bf
brombaut <ben.rombaut@unb.ca>
2020-12-24 08:18:56 -0400

    Adding goodreads bookshelf tests

---
+++ src/__tests__/GoodreadsBookshelf.test.ts
@@ -0,0 +1,30 @@
+import { GoodreadsBookshelf } from './../goodreads-bookshelf';
+import goodreadsconfig from '../config/goodreads-config';
+import { Book } from "../book";
+
+describe('GoodreadsApiFetcher class', () => {
+  let gb: GoodreadsBookshelf;
+
+  beforeEach(async () => {
+    gb = new GoodreadsBookshelf(goodreadsconfig.id, goodreadsconfig.key);
+  });
+
+  it('successfully constructs a GoodreadsBookshelf', () => {
+    expect(gb).toBeInstanceOf(GoodreadsBookshelf);
+  });
+
+  it('successfully builds a bookshelf', async () => {
+    await gb.getBooks();
+    expect(gb.readBooks()).not.toHaveLength(0)
+    gb.readBooks().forEach((book: Book) => {
+      expect(book).toBeInstanceOf(Book);
+    });
+    gb.readingBooks().forEach((book: Book) => {
+      expect(book).toBeInstanceOf(Book);
+    });
+
+    const groupedByYear: { [key: number]: Book[] } = gb.readBooksGroupedByYear();
+    expect(groupedByYear).toBeDefined();
+  });
+
+});
