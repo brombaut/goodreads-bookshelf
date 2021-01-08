98e54bbc87a1aaec55935947384154e1e2d6ee0a
brombaut <ben.rombaut@unb.ca>
2021-01-08 18:33:23 -0500

    0.2.0

--- src/__tests__/GoodreadsApiFetcher.test.ts
+++ src/__tests__/GoodreadsApiFetcher.test.ts
@@ -28,11 +28,13 @@ describe('GoodreadsApiFetcher class', () => {
   it('Gives a good error when the api request fails', () => {
     jest.setTimeout(15000);
     expect.assertions(1);
-    const failedGaf: GoodreadsApiFetcher = 
-      new GoodreadsApiFetcher(`${goodreadsconfig.id}_WRONG`, `${goodreadsconfig.key}_WRONG`);
+    const failedGaf: GoodreadsApiFetcher = new GoodreadsApiFetcher(
+      `${goodreadsconfig.id}_WRONG`,
+      `${goodreadsconfig.key}_WRONG`,
+    );
     expect.assertions(1);
     return failedGaf.fetch().catch((err: string) => {
-      expect(err).toBeTruthy()
+      expect(err).toBeTruthy();
     });
   });
 });
